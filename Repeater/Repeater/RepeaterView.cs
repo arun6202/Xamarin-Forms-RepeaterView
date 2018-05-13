cb92c76a24a2b88368089ebe6713275a0136db2d
Arun Balakrishnan <arun.balakrishnan@live.com>
2018-05-13 23:25:05 +0530

    sd

--- Repeater/Repeater/RepeaterView.cs
+++ Repeater/Repeater/RepeaterView.cs
@@ -6,93 +6,95 @@ using Xamarin.Forms;
 namespace XamarinForms.Plugin.Repeater
 {
 	public class RepeaterView : FlexLayout
-    {
+	{
 		private const int DefaultValue = 1;
 		public static readonly BindableProperty ItemTemplateProperty = BindableProperty.Create(
-            nameof(ItemTemplate), 
-            typeof(DataTemplate), 
-            typeof(RepeaterView), 
-            default(DataTemplate));
+			nameof(ItemTemplate),
+			typeof(DataTemplate),
+			typeof(RepeaterView),
+			default(DataTemplate));
 
-		public  static readonly BindableProperty ItemsSourceProperty = BindableProperty.Create(
-            nameof(ItemsSource), 
-            typeof(ICollection), 
-            typeof(RepeaterView), 
-            null, 
-            BindingMode.OneWay, 
-            propertyChanged: ItemsChanged);
-        
-        public RepeaterView()
-        {
+		public static readonly BindableProperty ItemsSourceProperty = BindableProperty.Create(
+			nameof(ItemsSource),
+			typeof(ICollection),
+			typeof(RepeaterView),
+			null,
+			BindingMode.OneWay,
+			propertyChanged: ItemsChanged);
+
+		public RepeaterView()
+		{
 			Direction = FlexDirection.Column;
-            AlignContent = FlexAlignContent.Center;
-            JustifyContent = FlexJustify.Center;
+			AlignContent = FlexAlignContent.Center;
+			JustifyContent = FlexJustify.Center;
 		}
 
 		public static readonly BindableProperty RepeatCountProperty = BindableProperty.Create(
-            nameof(ItemsSource),
-            typeof(int),
-            typeof(RepeaterView),
+			nameof(ItemsSource),
+			typeof(int),
+			typeof(RepeaterView),
 			DefaultValue,
-            BindingMode.OneWay,
-			propertyChanged: RepeatCountChanged);
+			BindingMode.OneWay
+			);
 
-		private static  void RepeatCountChanged(BindableObject bindable, object oldValue, object newValue)
-		{
-
-			var repeater = new RepeaterViewModel { RepeatCount = (int)newValue };
-           //	ItemsChanged(bindable, null, repeater.RepeatItems);
-
-		}
 
 		public int RepeatCount
-        {
-			get => (int)GetValue(RepeatCountProperty);
-			set => SetValue(RepeatCountProperty, value);
-        }
-        public ICollection ItemsSource
-        {
-            get => (ICollection) GetValue(ItemsSourceProperty);
-            set => SetValue(ItemsSourceProperty, value);
-        }
+		{
+			get
+			{
+				return (int)GetValue(RepeatCountProperty);
+			}
 
-        public DataTemplate ItemTemplate
-        {
-            get => (DataTemplate) GetValue(ItemTemplateProperty);
-            set => SetValue(ItemTemplateProperty, value);
-        }
-        
-        protected virtual View ViewFor(object item)
-        {
-            View view = null;
+			set
+			{
+				SetValue(RepeatCountProperty, value);
+				var repeater = new RepeaterViewModel { RepeatCount = value };
+				ItemsSource = repeater.RepeatItems;
+			}
+		}
+		public ICollection ItemsSource
+		{
+			get => (ICollection)GetValue(ItemsSourceProperty);
+			set => SetValue(ItemsSourceProperty, value);
+		}
 
-            if (ItemTemplate != null)
-            {
-                var content = ItemTemplate.CreateContent();
+		public DataTemplate ItemTemplate
+		{
+			get => (DataTemplate)GetValue(ItemTemplateProperty);
+			set => SetValue(ItemTemplateProperty, value);
+		}
 
-                view = content is View ? content as View : ((ViewCell)content).View;
+		protected virtual View ViewFor(object item)
+		{
+			View view = null;
 
-                view.BindingContext = item;
-            }
+			if (ItemTemplate != null)
+			{
+				var content = ItemTemplate.CreateContent();
 
-            return view;
-        }
-        
-        private static void ItemsChanged(BindableObject bindable, object oldValue, object newValue)
-        {
+				view = content is View ? content as View : ((ViewCell)content).View;
+
+				view.BindingContext = item;
+			}
+
+			return view;
+		}
+
+		private static void ItemsChanged(BindableObject bindable, object oldValue, object newValue)
+		{
 
 			if (!(bindable is RepeaterView control)) return;
 
 			control.Children.Clear();
 
-            var items = (ICollection) newValue;
+			var items = (ICollection)newValue;
 
-            if (items == null) return;
+			if (items == null) return;
 
-            foreach (var item in items)
-            {
-                control.Children.Add(control.ViewFor(new object()));
-            }
-        }
-    }
+			foreach (var item in items)
+			{
+				control.Children.Add(control.ViewFor(new object()));
+			}
+		}
+	}
 }
